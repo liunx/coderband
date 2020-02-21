51de8fb0233bd4baeb74d3ebd60bb1ee73eaaedd
liunx <liunx163@163.com>
2020-02-21 16:03:18 +0800

    Update.

--- tests/read_demo.py
+++ tests/read_demo.py
@@ -3,6 +3,8 @@
 from music21 import *
 import copy
 
+environment.set('musicxmlPath', '/usr/bin/musescore3')
+
 def read_xml():
     c = converter.parse('demo.xml')
     p = c.parts[0]
@@ -19,49 +21,46 @@ def tiny_note():
 
 
 
-def replaceNote(inNote, chordStateMap):
-    origChord = roman.RomanNumeral(origScaleDegree, key.Key(origNoteName))
-    currChord = roman.RomanNumeral(currScaleDegree, key.Key(currNoteName))
-
+def replaceNote(inNote, origChord, currChord, pitchMap):
+    pitchName = inNote.pitch.nameWithOctave
+    if pitchName in pitchMap:
+        inNote.pitch.nameWithOctave = pitchMap[pitchName]
+        return
     # find the closest note
     minSemi = 12
     semiVal = 0
+    distTable = []
+    semiValMap = {}
     for c in currChord:
         intval = interval.Interval(inNote, c)
         dist = abs(intval.semitones) % 12
-        if dist < minSemi:
-            minSemi = dist
-            if intval.semitones < 0:
-                ndist = -dist
-            else:
-                semiVal = dist
-    # check chordStateMap, avoid conflicts
-    if chordStateMap[inNote.name] == 0:
-        chordStateMap[inNote.name] = 1
-    else:
-        # recalculate the semitones
-        minSemi = 12
-        semiVal = 0
-        for c in currChord:
-            intval = interval.Interval(inNote, c)
-            dist = abs(intval.semitones) % 12
-            if dist < minSemi and dist != 0:
-                minSemi = dist
-                if intval.semitones < 0:
-                    ndist = -dist
-                else:
-                    ndist = dist
-    inNote.transpose(semiVal, inPlace=True)
+        distTable.append(dist)
+        if intval.semitones < 0:
+            semiVal = -dist
+        else:
+            semiVal = dist
+        semiValMap[dist] = semiVal
+
+    distTable.sort()
+    print(distTable)
+    for dist in distTable:
+        semiVal = semiValMap[dist]
+        tmpNote = inNote.transpose(semiVal)
+        pitchName = tmpNote.pitch.nameWithOctave
+        if pitchName not in pitchMap.values():
+            inNote.pitch.nameWithOctave = pitchName
+            pitchMap[inNote.pitch.nameWithOctave] = pitchName
+            break
 
 
-def replaceChord(inChord, chordStateMap):
+def replaceChord(inChord, origChord, currChord, pitchMap):
     oldChord = copy.deepcopy(inChord)
     # find the closest note
     for n in inChord:
-        replaceNote(n, chordStateMap)
+        replaceNote(n, origChord, pitchMap)
 
-origNoteName = 'C'
-currNoteName = 'G'
+origKey = 'C'
+currKey = 'G'
 origScaleDegree = 'I'
 currScaleDegree = 'I'
 
@@ -71,25 +70,23 @@ def chordProgress():
     p = s.parts[1]
     m = p[4]
     s = stream.Stream()
-    s.append(key.Key('C'))
+    s.append(key.Key(origKey))
     s.append(meter.TimeSignature('4/4'))
     s.append(clef.BassClef())
     s.append(copy.deepcopy(m))
-    origChord = roman.RomanNumeral(origScaleDegree, key.Key(origNoteName))
-    currChord = roman.RomanNumeral(currScaleDegree, key.Key(currNoteName))
+    origChord = roman.RomanNumeral(origScaleDegree, key.Key(origKey))
+    currChord = roman.RomanNumeral(currScaleDegree, key.Key(currKey))
 
-    chordStateMap = {}
-    for c in origChord:
-        chordStateMap[c.name] = 0
-
-    for i in m.iter:
-        if type(i) is note.Note:
-            replaceNote(i, chordStateMap)
-        elif type(i) is chord.Chord:
-            replaceChord(i, chordStateMap)
+    pitchMap = {}
+    if currKey != origKey:
+        for i in m.iter:
+            if type(i) is note.Note:
+                replaceNote(i, origChord, currChord, pitchMap)
+            elif type(i) is chord.Chord:
+                replaceChord(i, origChord, currChord, pitchMap)
 
     s.append(copy.deepcopy(m))
-    s.show('text')
+    s.show()
 
 
 if __name__ == "__main__":
