0de8ae10a9a541d95af38335d2ec49312ec30c4f
liunx <liunx163@163.com>
2020-03-05 18:32:01 +0800

    add chord 3to7/7to3 conversion

--- rhythmaker.py
+++ rhythmaker.py
@@ -61,12 +61,40 @@ def triadToTriad(fromChord, toChord):
                 chordMap[n.name] = diff.pop()
     return chordMap
 
+
 def triadToSeventh(fromChord, toChord):
+    triadIntervals = ['M-2', 'm-2', 'P1', 'm2', 'a1', 'M2']
     chordMap = {}
+    for n in fromChord.notes:
+        # step 1: the closest move
+        if n.name not in chordMap:
+            for i in triadIntervals:
+                tn = n.transpose(i)
+                if tn.name in toChord.pitchNames and tn.name not in chordMap.values():
+                    chordMap[n.name] = tn.name
+                    break
+        # step 2: fix missed notes
+        if n.name not in chordMap:
+            print("missed note: {}".format(n.name))
+            diff = set.difference(set(toChord.pitchNames), set(chordMap.values()))
+            if len(diff) == 1:
+                chordMap[n.name] = diff.pop()
+    # add missed notes
+    diff = set.difference(set(toChord.pitchNames), set(chordMap.values()))
+    if len(diff) == 1:
+        n = diff.pop()
+        if n == toChord.root().name:
+            print("Warning!!! Missed root note: {}".format(n))
+        elif n == toChord.seventh.name:
+            print("Warning!!! Missed seventh note: {}".format(n))
+        elif n == toChord.fifth.name:
+            print("Warning!!! Missed fifth note: {}".format(n))
+        elif n == toChord.third.name:
+            print("Missed third note: {}, can be ignored!".format(n))
     return chordMap
 
+
 def seventhToSeventh(fromChord, toChord):
-    seventhIntervals = ['m-3', 'M-2', 'm-2', 'P1', 'm2', 'a1', 'M2', 'm3']
     seventhIntervals = ['M-2', 'm-2', 'P1', 'm2', 'a1', 'M2']
     chordMap = {}
     for n in fromChord.notes:
@@ -88,7 +116,25 @@ def seventhToSeventh(fromChord, toChord):
 
 
 def seventhToTriad(fromChord, toChord):
-    pass
+    seventhIntervals = ['M-2', 'm-2', 'P1', 'm2', 'a1', 'M2']
+    chordMap = {}
+    for n in fromChord.notes:
+        # step 1: the closest move
+        if n.name not in chordMap:
+            for i in seventhIntervals:
+                tn = n.transpose(i)
+                if tn.name in toChord.pitchNames and tn.name not in chordMap.values():
+                    #print(n.pitch.nameWithOctave, tn.pitch.nameWithOctave)
+                    chordMap[n.name] = tn.name
+                    break
+        # step 2: fix missed notes
+        if n.name not in chordMap:
+            print("missed note: {}".format(n.name))
+            diff = set.difference(set(toChord.pitchNames), set(chordMap.values()))
+            if len(diff) == 1:
+                chordMap[n.name] = diff.pop()
+    return chordMap
+
 
 def chordsConnect(mode, scaleList, chordMapList):
     fromRN = None
@@ -109,6 +155,10 @@ def chordsConnect(mode, scaleList, chordMapList):
             chordMap = triadToTriad(fromRN, toRN)
         elif fromRN.isSeventh() and toRN.isSeventh():
             chordMap = seventhToSeventh(fromRN, toRN)
+        elif fromRN.isTriad() and toRN.isSeventh():
+            chordMap = triadToSeventh(fromRN, toRN)
+        elif fromRN.isSeventh() and toRN.isTriad():
+            chordMap = seventhToTriad(fromRN, toRN)
         else:
             print("Unfinished situation!")
             print("{} ==> {}".format(fromScale, toScale))
@@ -193,13 +243,13 @@ def outputSheet(sheetList):
 def demo02():
     ts1 = ['I', 'V', 'vi', 'iii', 'ii', 'V', 'I']
     ts2 = ['I7', 'V7', 'vi7', 'iii7', 'ii7', 'V7', 'I7']
-    ts3 = ['I', 'IV', 'V7', 'I']
+    ts3 = ['I', 'IV7', 'V', 'I']
     ts4 = ['I7', 'IV7', 'V7', 'I7']
     modeList = ['C', 'C#', 'D-', 'D', 'E-', 'E', 'F',
                 'F#', 'G-', 'G', 'A-', 'A', 'B-', 'B']
     modeList = ['C']
 
-    ts = ts2
+    ts = ts3
     inv = 0
     sheetLen = 5
 
